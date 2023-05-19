e068f8ab2150cfddb2aa108424877b3225f52900
AntonioLourencos <antonio.oficialcontato@gmail.com>
2023-05-19 20:40:02 -0300

    Initial Commit

---
+++ to-do/to_doApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_doApp.swift
+//  to-do
+//
+//  Created by Antonio Lourencos on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct to_doApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
